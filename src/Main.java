9dcfdbb2dfdc85518f7e1adbcb7dc45e9b93c73f
LinZichuan <linzichuan12@163.com>
2015-11-07 16:41:30 +0800

    Use Corpus file(CNKI_journal.txt) to Create Index

--- src/Main.java
+++ src/Main.java
@@ -28,13 +28,20 @@ public class Main {
 			Directory dir = FSDirectory.open(datadir);
 			IndexWriter writer = new IndexWriter(dir, new StandardAnalyzer(Version.LUCENE_35), true, IndexWriter.MaxFieldLength.LIMITED);
 			//构建Document并写入IndexWriter
-			File srcfile = new File("./data/data.txt");
+			File srcfile = new File("./data/CNKI_journal.txt");
 			StringBuffer buffer = new StringBuffer();
 			InputStreamReader isr = new InputStreamReader(new FileInputStream(srcfile), "GB2312");
 			BufferedReader br = new BufferedReader(isr); 
 			String ss;
 			ArrayList doc = new ArrayList();
+			int linenum = 0, block = 1;
 			while((ss=br.readLine()) != null) {
+				if (ss.equals("")) continue;
+				linenum ++;
+				if (linenum % 100000 == 0) {
+					block ++;
+					System.out.println(block);
+				}
 				if (ss.equalsIgnoreCase("<REC>")) {
 					if (doc.size() > 0) {
 						writer.addDocument((Document)doc.get(doc.size()-1));//将Document写入Index
@@ -42,6 +49,11 @@ public class Main {
 					doc.add(new Document());  //新建一个Document
 				}else {
 					int flag = ss.indexOf(">=");
+					if (flag <= 1) {
+						Document d = (Document) doc.get(doc.size()-1);
+						d.add(new Field("基金", ss, Field.Store.YES, Field.Index.ANALYZED));
+						continue;
+					}
 					String key = ss.substring(1, flag);
 					String val = ss.substring(flag+2, ss.length());
 					Document d = (Document) doc.get(doc.size()-1);
@@ -51,18 +63,18 @@ public class Main {
 			writer.close();
 			System.out.println("Index finish!");
 			//构建query
-			QueryParser parser = new QueryParser(Version.LUCENE_35, "篇名", new StandardAnalyzer(Version.LUCENE_35));
-			Query q = parser.parse("浙江的资本盛世");
+			QueryParser parser = new QueryParser(Version.LUCENE_35, "作者", new StandardAnalyzer(Version.LUCENE_35));
+			Query q = parser.parse("叶丽雅");
 			System.out.println(q);
 			//开始查询
 			IndexSearcher searcher = new IndexSearcher(dir);
-			TopDocs td = searcher.search(q, null, 100);
+			TopDocs td = searcher.search(q, null, 5);
 			int hitnum = td.totalHits;
 			System.out.println(hitnum);
 			ScoreDoc[] hits = td.scoreDocs;
 			for (int i = 0 ; i < hits.length; ++i) {
 				Document hitdoc = searcher.doc(hits[i].doc);
-				System.out.println(hitdoc.getField("中文刊名").stringValue());
+				System.out.println(hitdoc.getField("篇名").stringValue());
 			}
 		}catch(Exception e) {
 			e.printStackTrace();
