ae6fccda27792ae516c23496e04f1d15d4293e47
LinZichuan <linzichuan12@163.com>
2015-11-07 14:46:56 +0800

    Build index from corpus but cannot work on search

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
@@ -21,25 +26,46 @@ public class Main {
 			File datadir = new File("./data/index/");
 			Directory dir = FSDirectory.open(datadir);
 			IndexWriter writer = new IndexWriter(dir, new StandardAnalyzer(Version.LUCENE_35), true, IndexWriter.MaxFieldLength.LIMITED);
-			Document doc = new Document();  
-			doc.add(new Field("title", "Lucene introduction", Field.Store.YES, Field.Index.ANALYZED));  
-			doc.add(new Field("content", "Lucene1 works well", Field.Store.YES, Field.Index.ANALYZED));  
-			writer.addDocument(doc);
-			Document doc1 = new Document();  
-			doc1.add(new Field("title", "Lucene", Field.Store.YES, Field.Index.ANALYZED));  
-			doc1.add(new Field("content", "Lucene2 works well", Field.Store.YES, Field.Index.ANALYZED));  
-			writer.addDocument(doc1);
+			//构建Document并写入IndexWriter
+			File srcfile = new File("./data/data.txt");
+			StringBuffer buffer = new StringBuffer();
+			InputStreamReader isr = new InputStreamReader(new FileInputStream(srcfile), "GB2312");
+			BufferedReader br = new BufferedReader(isr); 
+			String ss;
+			ArrayList doc = new ArrayList();
+			while((ss=br.readLine()) != null) {
+				if (ss.equalsIgnoreCase("<REC>")) {
+					if (doc.size() > 0) {
+						writer.addDocument((Document)doc.get(doc.size()-1));//将Document写入Index
+					}
+					doc.add(new Document());  //新建一个Document
+				}else {
+					int flag = ss.indexOf(">=");
+					String key = ss.substring(1, flag);
+					String val = ss.substring(flag+2, ss.length());
+					//System.out.println(key + "," + val);
+					Document d = (Document) doc.get(doc.size()-1);
+					d.add(new Field(key, val, Field.Store.YES, Field.Index.ANALYZED));
+				}
+			}
+			Document dd = new Document();
+			dd.add(new Field("CN", "11-3928/TN", Field.Store.YES, Field.Index.ANALYZED));
+			dd.add(new Field("中文刊名", "hahaha", Field.Store.YES, Field.Index.ANALYZED));
+			writer.addDocument(dd);
 			writer.close();
 			System.out.println("Index finish!");
+		
 			IndexSearcher searcher = new IndexSearcher(dir);
-			Term t = new Term("title", "introduction");
+			Term t = new Term("CN", "11-3928/TN");
 			Query q = new TermQuery(t);
+			System.out.println(q);
 			TopDocs td = searcher.search(q, null, 100);
 			int hitnum = td.totalHits;
+			System.out.println(hitnum);
 			ScoreDoc[] hits = td.scoreDocs;
 			for (int i = 0 ; i < hits.length; ++i) {
 				Document hitdoc = searcher.doc(hits[i].doc);
-				System.out.println(hitdoc.getField("content").stringValue());
+				System.out.println(hitdoc.getField("中文刊名").stringValue());
 			}
 		}catch(Exception e) {
 			e.printStackTrace();
