02212be62a72d5abc48cb1e8e7f6c9fa35ded1b0
LinZichuan <linzichuan12@163.com>
2015-11-09 22:54:15 +0800

    Use IKAnalyzer in creating index and parsing query

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.Term;
+import org.apache.lucene.queryParser.MultiFieldQueryParser;
 import org.apache.lucene.queryParser.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
@@ -19,14 +22,26 @@ import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
+import org.wltea.analyzer.lucene.IKAnalyzer;
 
-public class Main {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Main extends HttpServlet{
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		System.out.println("haha");
+		String input = request.getParameter("keyword");
+		System.out.println(input);
+	}
 	public static void main(String []args) {
 		System.out.println("Start!");
 		try{
 			File datadir = new File("./data/index/");
 			Directory dir = FSDirectory.open(datadir);
-			IndexWriter writer = new IndexWriter(dir, new StandardAnalyzer(Version.LUCENE_35), true, IndexWriter.MaxFieldLength.LIMITED);
+			IndexWriter writer = new IndexWriter(dir, new IKAnalyzer(), true, IndexWriter.MaxFieldLength.LIMITED);
 			//构建Document并写入IndexWriter
 			File srcfile = new File("./data/CNKI_journal.txt");
 			StringBuffer buffer = new StringBuffer();
@@ -63,18 +78,19 @@ public class Main {
 			writer.close();
 			System.out.println("Index finish!");
 			//构建query
-			QueryParser parser = new QueryParser(Version.LUCENE_35, "作者", new StandardAnalyzer(Version.LUCENE_35));
+			QueryParser parser = new MultiFieldQueryParser(Version.LUCENE_35, new String[]{"作者", "篇名"}, new IKAnalyzer());
 			Query q = parser.parse("创业");
 			System.out.println(q);
 			//开始查询
 			IndexSearcher searcher = new IndexSearcher(dir);
-			TopDocs td = searcher.search(q, null, 5);
+			TopDocs td = searcher.search(q, null, 100);
 			int hitnum = td.totalHits;
 			System.out.println(hitnum);
 			ScoreDoc[] hits = td.scoreDocs;
 			for (int i = 0 ; i < hits.length; ++i) {
 				Document hitdoc = searcher.doc(hits[i].doc);
-				System.out.println(hitdoc.getField("作者").stringValue());
+				if (hitdoc.getField("篇名") != null)
+					System.out.println(hitdoc.getField("篇名").stringValue());
 			}
 		}catch(Exception e) {
 			e.printStackTrace();
