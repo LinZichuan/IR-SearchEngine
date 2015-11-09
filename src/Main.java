9029a0e18f19dd07558879bc9623621918704f9c
LinZichuan <linzichuan12@163.com>
2015-11-09 23:39:10 +0800

    Reconfig tomcat server successfully

--- src/Main.java
+++ src/Main.java
@@ -30,7 +30,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class Main extends HttpServlet{
-	@Override
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 		System.out.println("haha");
 		String input = request.getParameter("keyword");
